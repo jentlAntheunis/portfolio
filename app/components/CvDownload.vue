8508cf5efd60fc994978dde34e42df9760e01a00
jentlAntheunis <jentl.antheunis@student.howest.be>
2026-04-06 15:04:16 +0100

    cv download button

--- app/components/CvDownload.vue
+++ app/components/CvDownload.vue
@@ -1,5 +1,30 @@
-<script></script>
+<script setup lang="ts">
+const cvUrl = "/Jentl_Antheunis_CV.pdf";
+</script>
 <template>
-	<h4 id="cv-download">Download CV</h4>
+	<a id="cv-download" class="download-btn" :href="cvUrl" download>Download CV</a>
 </template>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@use "@/assets/css/base.scss" as *;
+
+.download-btn {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	color: #fff;
+	text-decoration: none;
+	font-weight: 500;
+	background-color: $color-primary;
+	padding: 0.5rem 1rem;
+	border-radius: 4px;
+	cursor: pointer;
+	margin-bottom: 2rem;
+	margin-top: 1rem;
+
+	transition: background-color 0.3s ease;
+
+	&:hover {
+		background-color: darken($color-primary, 10%);
+	}
+}
+</style>
